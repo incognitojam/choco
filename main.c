6eb3ca4338d3483b01f06c7bd845fc4ef494d1e9
Cameron Clough <cameronjclough@gmail.com>
2025-09-11 16:13:41 +0100

    tokenize a file (#1)

--- main.c
+++ main.c
@@ -3,25 +3,35 @@
 #include "tokenizer.h"
 
 int main() {
-  TokenList *list = createTokenList();
+  TokenList *toks = createTokenList();
 
+  /*
   Token *var = createToken(TOKEN_VAR);
-  addToken(list, var);
+  addToken(toks, var);
 
   Token *ident = createToken(TOKEN_IDENTIFIER);
   ident->identifier.name = strdup("x");
-  addToken(list, ident);
+  addToken(toks, ident);
 
   Token *eq = createToken(TOKEN_EQUALS);
-  addToken(list, eq);
+  addToken(toks, eq);
 
   Token *value = createToken(TOKEN_NUMBER);
   value->number.value = 0;
-  addToken(list, value);
+  addToken(toks, value);
 
   Token *semicolon = createToken(TOKEN_SEMICOLON);
-  addToken(list, semicolon);
+  addToken(toks, semicolon);
+  */
 
-  dumpTokenList(list);
-  freeTokenList(list);
+  FILE *file = fopen("./examples/vars.js", "r");
+  if (file == NULL) {
+    perror("Error opening file");
+    return 1;
+  }
+  tokenizeFile(file, toks);
+  fclose(file);
+
+  dumpTokenList(toks);
+  freeTokenList(toks);
 }
--- tokenizer.h
+++ tokenizer.h
@@ -1,3 +1,6 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +9,10 @@ typedef enum TokenType {
   TOKEN_VAR,
   TOKEN_IDENTIFIER,
   TOKEN_EQUALS,
+  TOKEN_PLUS,
+  TOKEN_MINUS,
+  TOKEN_MULTIPLY,
+  TOKEN_DIVIDE,
   TOKEN_NUMBER,
   TOKEN_STRING,
   TOKEN_SEMICOLON,
@@ -74,6 +81,18 @@ void dumpTokenList(TokenList *list) {
     case TOKEN_EQUALS:
       printf("EQUALS ");
       break;
+    case TOKEN_PLUS:
+      printf("PLUS ");
+      break;
+    case TOKEN_MINUS:
+      printf("MINUS ");
+      break;
+    case TOKEN_MULTIPLY:
+      printf("MULTIPLY ");
+      break;
+    case TOKEN_DIVIDE:
+      printf("DIVIDE ");
+      break;
     case TOKEN_NUMBER:
       printf("NUMBER %f ", token->number.value);
       break;
@@ -93,10 +112,121 @@ void freeTokenList(TokenList *list) {
   TokenNode *node = list->head;
   while (node != NULL) {
     TokenNode *next = node->next;
-    free(node->token->identifier.name);
+    if (node->token->type == TOKEN_IDENTIFIER) {
+      free(node->token->identifier.name);
+    } else if (node->token->type == TOKEN_STRING) {
+      free(node->token->string.value);
+    }
     free(node->token);
     free(node);
     node = next;
   }
   free(list);
+}
+
+void tokenizeFile(FILE *input, TokenList *list) {
+  int c;
+  while ((c = fgetc(input)) != EOF) {
+    if (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
+      // skip whitespace
+      continue;
+    }
+
+    if (c == ';') {
+      addToken(list, createToken(TOKEN_SEMICOLON));
+      continue;
+    }
+    if (c == '=') {
+      addToken(list, createToken(TOKEN_EQUALS));
+      continue;
+    }
+    if (c == '+') {
+      addToken(list, createToken(TOKEN_PLUS));
+      continue;
+    }
+    if (c == '-') {
+      addToken(list, createToken(TOKEN_MINUS));
+      continue;
+    }
+    if (c == '*') {
+      addToken(list, createToken(TOKEN_MULTIPLY));
+      continue;
+    }
+    if (c == '/') {
+      addToken(list, createToken(TOKEN_DIVIDE));
+      continue;
+    }
+
+    if (isdigit(c)) {
+      ungetc(c, input);
+      char buffer[256];
+      int i = 0;
+      bool hasDecimal = false;
+      while ((c = fgetc(input)) != EOF && i < (sizeof(buffer) - 1) &&
+             (isdigit(c) || (!hasDecimal && c == '.'))) {
+        if (c == '.') {
+          hasDecimal = true;
+        }
+        buffer[i++] = c;
+      }
+      buffer[i] = '\0';
+      if (c != EOF) {
+        ungetc(c, input);
+      }
+
+      Token *token = createToken(TOKEN_NUMBER);
+      token->number.value = atof(buffer);
+      addToken(list, token);
+      continue;
+    }
+
+    if (isalpha(c)) {
+      ungetc(c, input);
+      char buffer[256];
+      int i = 0;
+      while ((c = fgetc(input)) != EOF && i < (sizeof(buffer) - 1) &&
+             (isalpha(c) || isdigit(c) || c == '_')) {
+        buffer[i++] = c;
+      }
+      buffer[i] = '\0';
+      if (c != EOF) {
+        ungetc(c, input);
+      }
+
+      char *name = (char *)malloc(strlen(buffer) + 1);
+      strcpy(name, buffer);
+
+      Token *token;
+      if (strcmp("var", name) == 0) {
+        token = createToken(TOKEN_VAR);
+        free(name);
+      } else {
+        token = createToken(TOKEN_IDENTIFIER);
+        token->identifier.name = name;
+      }
+      addToken(list, token);
+      continue;
+    }
+
+    if (c == '"') {
+      char buffer[1024];
+      int i = 0;
+      while ((c = fgetc(input)) != EOF && i < (sizeof(buffer) - 1) &&
+             c != '"') {
+        buffer[i++] = c;
+      }
+      buffer[i] = '\0';
+
+      char *value = (char *)malloc(strlen(buffer) + 1);
+      strcpy(value, buffer);
+
+      Token *token = createToken(TOKEN_STRING);
+      token->string.value = value;
+      addToken(list, token);
+      continue;
+    }
+
+    printf("Unexpected character: %c\n", c);
+    assert(false);
+  }
 }
